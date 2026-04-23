3b5e6e0d0a94d220474affb61b6536d693eb522a
oRonold <or040918@gmail.com>
2026-04-23 14:51:43 -0300

    implementa método contagemPorFuncao

--- src/main/java/br/com/duxusdesafio/service/ApiService.java
+++ src/main/java/br/com/duxusdesafio/service/ApiService.java
@@ -146,8 +146,17 @@ public class ApiService {
      * Vai retornar o número (quantidade) de Funções dentro do período
      */
     public Map<String, Long> contagemPorFuncao(LocalDate dataInicial, LocalDate dataFinal, List<Time> todosOsTimes){
-        // TODO Implementar método seguindo as instruções!
-        return null;
+        return todosOsTimes.stream()
+                // Filtro para os times nas datas podendo ser null ou  exatamente dentro do período
+                .filter(time -> dataInicial == null || time.getData().isAfter(dataInicial))
+                .filter(time -> dataFinal == null || time.getData().isBefore(dataFinal))
+
+                // Pego todas as funções dos jogadores
+                .flatMap(time -> time.getComposicaoTime().stream())
+                .map(composicaoTime -> composicaoTime.getIntegrante().getFuncao())
+
+                // Retorno um Map com a função e a quantidade de aparições
+                .collect(Collectors.groupingBy(funcao -> funcao, Collectors.counting()));
     }
 
 }
